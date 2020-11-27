a109986b54eb758a822dc01a534c66edd76efb0e
James Butterworth <jjbsports16@hotmail.co.uk>
2020-11-27 21:30:37 +0000

    Added very basic very ugly code GD

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,52 @@ mod neural_net;
 
 use neural_net::Neuron;
 
+fn mean_squared_error(output: f64, target: f64) -> f64 {
+    (output - target).powi(2)
+}
+
+fn output_gradient(output: f64, target: f64) -> f64 {
+    2. * (output - target)
+}
+
+fn gradient_descent(neuron: &mut Neuron) {
+
+    let learning_rate = 0.01;
+    for i in 0..neuron.weights.len() {
+        neuron.weights[i] -= learning_rate * neuron.gradients[i];
+    }
+
+}
+
 fn main() {
 
-    let weights = vec![1., 2., 3.];
+    //let weights = vec![1., 2., 3.];
+    let weights = vec![-2.];
 
-    let neuron = Neuron::new(weights);
+    let mut neuron = Neuron::new(weights);
 
     println!("{:?}", neuron);
 
-    let inputs = vec![1., 2., 1.];
-    let output = neuron.forward(inputs);
+    let inputs = vec![2.];
 
-    println!("{}", output);
+    let num_epochs = 100;
+    for i in 0..num_epochs {
+        let output = neuron.forward(&inputs);
 
+        //println!("Output: {}", output);
+        let target = 1.;
+        let loss = mean_squared_error(output, target);
+
+        println!("Loss: {}", loss);
+
+        neuron.backward(output_gradient(output, target));
+
+        //println!("{:?}", neuron);
+
+        gradient_descent(&mut neuron);
+
+        println!("{:?}", neuron);
+        neuron.zero_grad();
+
+    }
 }
--- src/neural_net.rs
+++ src/neural_net.rs
@@ -1,27 +1,52 @@
 #[derive(Debug)]
 pub struct Neuron {
     
-    weights: Vec<f64>,
+    pub weights: Vec<f64>,
+
+    pub gradients: Vec<f64>,
+    
+    //Keep track of the inputs on the forward pass to be used in the 
+    //gradient calculation
+    inputs: Vec<f64>,
 
 }
 
 impl Neuron {
 
     pub fn new(weights: Vec<f64>) -> Self {
+        let gradients = vec![0.; weights.len()];
+        let inputs = vec![0.; weights.len()];
         Neuron {
             weights,
+            gradients,
+            inputs,
         }
     }
 
-    pub fn forward(&self, inputs: Vec<f64>) -> f64 {
+    pub fn forward(&mut self, inputs: &Vec<f64>) -> f64 {
 
         let mut sum = 0.;
         for i in 0..inputs.len() {
             sum += inputs[i] * self.weights[i];
         }
 
+        self.inputs = inputs.to_vec();
+
         sum
 
+    }
+
+    //Computes the gradients
+    pub fn backward(&mut self, output_grad: f64) {
+        for i in 0..self.gradients.len() {
+            self.gradients[i] = self.inputs[i] * output_grad;
+        }
+    }
+
+    pub fn zero_grad(&mut self) {
+        for grad in &mut self.gradients {
+            *grad = 0.;
+        }
     }
 
 }
